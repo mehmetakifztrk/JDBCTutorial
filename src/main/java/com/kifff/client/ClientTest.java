4f1555627b17effceaef510482af67943dc2d030
mehmetakifztrk <mehmetakifztrk@outlook.com>
2019-02-01 22:55:51 +0300

    bug fix

--- src/main/java/com/kifff/client/ClientTest.java
+++ src/main/java/com/kifff/client/ClientTest.java
@@ -3,6 +3,7 @@ package com.kifff.client;
 import com.kifff.util.DBUtil;
 
 import java.sql.Connection;
+import java.sql.Statement;
 
 /**
  * Hello world!
@@ -11,9 +12,36 @@ import java.sql.Connection;
 public class ClientTest
 {
     public static void main( String[] args ) {
-        Connection connection = DBUtil.getConnection();
-        if (connection!=null){
-            System.out.println("1- JDBC bağlandı");
+       //createEmployee();
+        updateEmployeeEmailById();
+    }
+
+    private static void updateEmployeeEmailById() {
+        try(Connection connection = DBUtil.getConnection();Statement st = connection.createStatement();){
+            String SQLUPDATE = "UPDATE employee_table  SET email='mehmetakifztrk44@gmail.com' WHERE employee_id=1";
+            int executeUpdate = st.executeUpdate(SQLUPDATE); // sorgu çalisirsa 1 calismassa 0
+            if (executeUpdate == 1){
+                System.out.println("basarili");
+            }else{
+                System.out.println("basarisiz");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void createEmployee() {
+        try(Connection connection = DBUtil.getConnection();Statement st = connection.createStatement();){
+            String SQLINSTERT = "INSERT INTO employee_table (employee_name,email,salary,date_of_joining,bonus) " +
+                    "VALUES ('Mehmet Akif Öztürk','mehmetakifztrk@outlook.com',60000.00,'2019-05-17',400.00)";
+            int executeUpdate = st.executeUpdate(SQLINSTERT); // sorgu çalisirsa 1 calismassa 0
+            if (executeUpdate == 1){
+                System.out.println("basarili");
+            }else{
+                System.out.println("basarisiz");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
