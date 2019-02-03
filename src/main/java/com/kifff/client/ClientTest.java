b36ad9fc25eaf9f8cd7b50da73b401a338126f25
mehmetakifztrk <mehmetakifztrk@outlook.com>
2019-02-03 12:32:13 +0300

    CRUD Operation using JDBC PreparedStatement

--- src/main/java/com/kifff/client/ClientTest.java
+++ src/main/java/com/kifff/client/ClientTest.java
@@ -1,11 +1,16 @@
 package com.kifff.client;
 
+import com.kifff.dao.EmployeeDAO;
+import com.kifff.dao.impl.EmployeeDAOImpl;
+import com.kifff.model.Employee;
 import com.kifff.util.DBUtil;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Hello world!
@@ -14,26 +19,37 @@ import java.sql.Statement;
 public class ClientTest
 {
     public static void main( String[] args ) {
-        getEmployeeById();
+        EmployeeDAO employeeDAO = new EmployeeDAOImpl();
+      //  Employee employee = getEmployee();
+      //  employeeDAO.createEmployee(employee);
+          Employee employee = employeeDAO.getEmployeeByID(1);
+        System.out.println(employee.toString());
+      //  employeeDAO.updateEmployeeEmailById("mehmetakifztrk@outlook.com",1);
+      //  employeeDAO.deleteEmployeeById(3);
+       // getAllEmployeesInfo(employeeDAO);
     }
 
-    private static void getEmployeeById() {
-        try(Connection connection = DBUtil.getConnection();Statement st = connection.createStatement();){
-            String SQL = "SELECT * FROM employee_table WHERE employee_id=1";
-            ResultSet rs = st.executeQuery(SQL);
-            if (rs.next()){
-                int empId = rs.getInt("employee_id");
-                String eName = rs.getString("employee_name");
-                String email = rs.getString("email");
-                Double salary = rs.getDouble("salary");
-                BigDecimal bonus = rs.getBigDecimal("bonus");
+    private static void getAllEmployeesInfo(EmployeeDAO employeeDAO) {
+        List<Employee> empList = employeeDAO.getAllEmployeeInfo();
+        empList.forEach(System.out::println);
+    }
 
-                System.out.println(empId+"/n"+eName+"/n"+email+"/n"+salary+"/n"+bonus);
-            }else{
-                System.out.println("bu id de kayıt yok");
-            }
-        }catch (Exception e){
-            e.printStackTrace();
+    private static void getEmployeeById(EmployeeDAO employeeDAO) {
+        Employee employee2 = employeeDAO.getEmployeeByID(1);
+        if (employee2 != null){
+            System.out.println(employee2.toString());
+        }else {
+            System.out.println("hataa");
         }
+    }
+
+    private static Employee getEmployee(){
+        Employee employee = new Employee();
+        employee.setEmployeeName("Akif");
+        employee.setEmail("mehmetakifztrk@outlook.com");
+        employee.setSalary(new Double(4000.00));
+        employee.setBonus(new BigDecimal(290));
+        employee.setDoj(new Date());
+        return employee;
     }
 }
